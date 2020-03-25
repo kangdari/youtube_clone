72e0db0a615fe7c1fb4e51643a720a2766b4ecb3
kangdari_ <ksh44820@naver.com>
2020-03-25 14:13:52 +0900

    user가 자신의 video는 구독하지 못하도록 구독 버튼 설정

--- client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -1,18 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { Row, Col, List, Avatar } from "antd";
 import axios from "axios";
-import SideVideo from './SideVideo';
-import Subscribe from './Subscribe';
+import SideVideo from "./SideVideo";
+import Subscribe from "./Subscribe";
 
 const VideoDetailPage = ({ match }) => {
   const [videoDetail, setVideoDetail] = useState([]);
-
 
   useEffect(() => {
     // 파라미터 읽기
     const videoId = match.params.videoID;
     const variable = { videoId };
-  
+
     axios.post("/api/video/getVideoDetail", variable).then(response => {
       if (response.data.success) {
         // console.log(response.data);
@@ -25,6 +24,13 @@ const VideoDetailPage = ({ match }) => {
 
   // videoDetail.writer 로딩 여부에 따라 렌더링을 다르게 설정
   if (videoDetail.writer) {
+    // 로그인 user가 자신이 업로드한 video는 구독하지 못하도록 설정
+    // 즉, 현재 로그인 user id와 video 게시자의 id가 같지 않을때 구독 버튼이 보이도록 설정
+    const subscribeButton = videoDetail.writer._id !==
+      JSON.parse(localStorage.getItem("auth"))._id && (
+      <Subscribe userTo={videoDetail.writer._id} />
+    );
+
     return (
       <Row gutter={[16, 16]}>
         <Col lg={18} xs={24}>
@@ -39,7 +45,7 @@ const VideoDetailPage = ({ match }) => {
             <List.Item
               // 좋아요 싫어요
               // userTo: 해당 비디오 작성자의 id 값을 props로 전달
-              actions={[<Subscribe userTo={videoDetail.writer._id} />]}
+              actions={[subscribeButton]}
             >
               <List.Item.Meta
                 // writer.image 필요
