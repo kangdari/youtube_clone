4a43566eed1c76482745282cd8c9b6b0e2d13de4
kangdari_ <ksh44820@naver.com>
2020-03-26 14:23:13 +0900

    Like && DisLike Model 생성
    ex) like
    case 1: 클릭되지 않은 상태에서 클릭한 경우: 좋아요 +1
    case 2: 이미 좋아요가 클릭되어있는 상태: 좋아요 -1
    case 3: 싫어요 버튼이 눌러진 상태에서 좋아요 클릭: 싫어요 -1, 좋아요 +1

---
+++ client/src/components/views/VideoDetailPage/SingleComment.js
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import { Comment, Avatar, Button, Input } from "antd";
+import axios from "axios";
+import { useSelector } from 'react-redux';
+
+const { TextArea } = Input;
+
+const SingleComment = ({ videoId, comment, refreshComments }) => {
+  const [visible, setVisible] = useState(false);
+  const [commentValue, setCommentValue] = useState("");
+  const { auth } = useSelector(state => ({
+    auth: state.auth.auth,
+  }))
+
+  // Reply form 렌더링 여부
+  const openReply = () => {
+    setVisible(!visible);
+  };
+
+  const handleChange = e => {
+    setCommentValue(e.currentTarget.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    let variables = {
+      content: commentValue,
+      // writer: JSON.parse(localStorage.getItem('auth'))._id, // local 저장소에서 가져온 id 값
+      writer: auth._id,
+      videoId,
+      responseTo: comment._id //
+    };
+
+    // comment 저장 api
+    axios.post("/api/comment/saveComment", variables).then(response => {
+      if (response.data.success) {
+        // console.log(response.data.comment);
+        
+        // 전체 comments 상태 업데이트
+        refreshComments(response.data.comment);
+        setCommentValue('');
+        setVisible(!visible)
+      } else {
+        alert("Failed save comment");
+      }
+    });
+  };
+
+  const actions = [
+    <span onClick={openReply} key="comment-basic-reply-to">
+      Reply to
+    </span>
+  ];
+
+  return (
+    <div>
+      <Comment
+        actions={actions}
+        author={comment.writer.name}
+        content={<p>{comment.content}</p>}
+        avatar={<Avatar src={comment.writer.image} alt="user-img" />}
+      />
+
+      {/* true일 때만 보이도록 설정 */}
+      {visible && (
+        <form style={{ display: "flex" }} onSubmit={handleSubmit}>
+          <TextArea
+            style={{ width: "100%", borderRadius: "5px" }}
+            onChange={handleChange}
+            value={commentValue}
+            placeholder="Enter comment..."
+          />
+          <button style={{ width: "20%" }} type="submit">
+            Submit
+          </button>
+        </form>
+      )}
+    </div>
+  );
+};
+
+export default SingleComment;
