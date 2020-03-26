8f126758835868cd21e2156ce321c45135d0e139
kangdari_ <ksh44820@naver.com>
2020-03-26 16:14:18 +0900

    likes, dislikes 정보 가져오기 server/routes/like.js: /getLikes, /getDisLikes에서 각각 좋아요, 싫어요 정보를 클라이언트에 응답.
    LikeDisLike 컴포넌트: 서버 응답 성공 시 해당 video의 좋아요, 싫어요 개수 상태 저장 및 현재 user가 해당 video를 이미 좋아요, 싫어요를
    눌렀는지 상태 확인, 그리고 상태 값에 따른 렌더링 설정
    
    서버에 Request를 보낼 때 video와 comment 인자 값을 다르게 보내야한다.

--- client/src/components/views/VideoDetailPage/LikeDisLike.js
+++ client/src/components/views/VideoDetailPage/LikeDisLike.js
@@ -1,9 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Tooltip } from 'antd';
 import { DislikeOutlined, LikeOutlined, LikeFilled, DislikeFilled } from "@ant-design/icons";
 
 const LikeDisLike = ({ video, videoId, userId, commentId }) => {
+    const [likes, setLikes] = useState(0); // 좋아요 개수
+    const [liked, setLiked] = useState(false); // 좋아요 상태
+    
+    const [dislikes, setDislikes] = useState(0); // 싫어요 개수
+    const [disliked, setDisliked] = useState(false); // 싫어요 상태
+
+
     let variable = {};
     // video의 좋아요, 싫어요
     if(video) {
@@ -19,10 +26,46 @@ const LikeDisLike = ({ video, videoId, userId, commentId }) => {
             .then(response => { 
                 if(response.data.success){
                     
+                    // 해당 video의 총 좋아요 개수
+                    setLikes(response.data.likes.length);
+                    
+                    // 현재 user가 해당 video를 이미 좋아요를 눌렀는지
+                    response.data.likes.map(like => {
+                        // like의 userId와 현재 user의 id를 비교
+                        if(like.userId === userId){
+                            setLiked(true); // 이미 좋아요 누른 상태
+                        }else{
+                            setLiked(false);
+                        }
+                    })
+
                 }else {
-                    alert('Failed load like && dislike')
+                    alert('Failed load likes');
                 }
             })
+
+        axios.post('/api/like/getDisLikes', variable)
+            .then(response => {
+                if(response.data.success){
+
+                    // 해당 video의 총 싫어요 개수
+                    setDislikes(response.data.dislikes.length);
+
+                    // 현재 user가 해당 video를 이미 싫어요를 눌렀는지
+                    response.data.dislikes.map(dislike => {
+                        // dislike의 userId와 현재 user의 id 비교
+                        if(dislike.userId === userId){
+                            setDisliked(true); // 이미 싫어요 누른 상태
+                        }else{
+                            setDisliked(false);
+                        }
+                    })
+
+                }else{
+                    alert('Failed load dislikes');
+                }
+            })
+        
     })
 
     return (
@@ -30,20 +73,18 @@ const LikeDisLike = ({ video, videoId, userId, commentId }) => {
             {/* 좋아요 */}
             <span key="comment-basic-like">
                 <Tooltip title="Like">
-                    <LikeOutlined onClick/>
-                    {/* <LikeFilled onClick/> */}
+                    { liked ? <LikeFilled onClick/> : <LikeOutlined onClick/> }
                 </Tooltip>
             </span>
-            <span style={{ paddingLeft: '0.5rem', cursor: 'auto'}}> 1 </span>
+            <span style={{ paddingLeft: '0.5rem', cursor: 'auto'}}> {likes} </span>
 
             {/* 싫어요 */}
             <span key="comment-basic-dislike">
                 <Tooltip title="Dislike">
-                    <DislikeOutlined onClick/>
-                    {/* <DislikeFilled onClick/> */}
+                    { disliked ? <DislikeFilled onClick/> : <DislikeOutlined onClick/> }
                 </Tooltip>
             </span>
-            <span style={{ paddingLeft: '0.5rem', cursor: 'auto'}}> 1 </span>
+            <span style={{ paddingLeft: '0.5rem', cursor: 'auto'}}> {dislikes} </span>
 
         </div>
     );
--- server/routes/like.js
+++ server/routes/like.js
@@ -11,16 +11,34 @@ router.post("/getLikes", (req, res) => {
   // video
   if (videoId) {
     variable = { videoId };
-  // comment
+    // comment
   } else {
     variable = { commentId };
   }
 
-  Like.find(variable)
-    .exec((err, likes) => {
-        if(err) return res.status(400).json({ success: false, err });
-        return res.status(200).json({ success: true, likes }); 
-    })
+  Like.find(variable).exec((err, likes) => {
+    if (err) return res.status(400).json({ success: false, err });
+    return res.status(200).json({ success: true, likes });
+  });
+});
+
+router.post("/getDisLikes", (req, res) => {
+  const { userId, videoId, commentId } = req.body;
+
+  let variable = {};
+
+  // video
+  if (videoId) {
+    variable = { videoId };
+    // comment
+  } else {
+    variable = { commentId };
+  }
+
+  DisLike.find(variable).exec((err, dislikes) => {
+      if(err) return res.status(400).json({ success: false, err});
+      return res.status(200).json({ success: true, dislikes });
+  });
 });
 
 module.exports = router;
